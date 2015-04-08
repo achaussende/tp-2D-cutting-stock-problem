25a6bf59210b958247bae930c298bd980de05436
Slashgear <antoine395.caron@laposte.net>
2015-04-08 21:55:42 +0200

    ACN: Add IncrementNeighbour generation and Test

--- Core/src/main/java/com/polytech4A/cuttingstock/core/solver/neighbour/IncrementNeighbour.java
+++ Core/src/main/java/com/polytech4A/cuttingstock/core/solver/neighbour/IncrementNeighbour.java
@@ -1,24 +1,29 @@
 /*
- * Project to resolve 2D cutting stock problem for Discreet Optimizations course at Polytech Lyon
- * Copyright (C) 2015.  CARON Antoine and CHAUSSENDE Adrien
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ *  * Project to resolve 2D cutting stock problem for Discreet Optimizations course at Polytech Lyon
+ *  * Copyright (C) 2015.  CARON Antoine and CHAUSSENDE Adrien
+ *  *
+ *  * This program is free software: you can redistribute it and/or modify
+ *  * it under the terms of the GNU Affero General Public License as
+ *  * published by the Free Software Foundation, either version 3 of the
+ *  * License, or (at your option) any later version.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  * GNU Affero General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU Affero General Public License
+ *  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.polytech4A.cuttingstock.core.solver.neighbour;
 
+import com.polytech4A.cuttingstock.core.model.Box;
+import com.polytech4A.cuttingstock.core.model.Pattern;
 import com.polytech4A.cuttingstock.core.model.Solution;
+import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
 
@@ -31,11 +36,77 @@ import java.util.ArrayList;
  *          Generator of neighbours using box addition to a pattern.
  */
 public class IncrementNeighbour implements INeighbourUtils {
+
+    private static final Logger logger = Logger.getLogger(IncrementNeighbour.class);
+
     /**
      * {@inheritDoc}
      */
     @Override
-    public ArrayList<Solution> getNeighbourhood(Solution s) {
-        return null;
+    public ArrayList<Solution> getNeighbourhood(final Solution s) {
+        ArrayList<Solution> solutions = new ArrayList<>();
+        solutions.addAll(this.getAdditiveNeighbourhood(s));
+        solutions.addAll(this.getRemoveNeighbourhood(s));
+        return solutions;
+    }
+
+    @Override
+    public Solution getModifications(final Solution solution) {
+        Solution cpSolution = null;
+        cpSolution = solution.clone();
+        cpSolution.getPatterns().parallelStream().forEach(p -> {
+            p.getAmounts().parallelStream().forEach(b -> {
+                if (b.getAmount() > 0) {
+                    b.setAmount(1);
+                }
+            });
+        });
+        return cpSolution;
+    }
+
+    /**
+     * Generation of +1 solutions
+     *
+     * @param solution solution to analyse
+     * @return Arraylist of solution containing combinations of +1 solution.
+     */
+    private ArrayList<Solution> getAdditiveNeighbourhood(final Solution solution) {
+        Solution cpSolution = solution.clone();
+        ArrayList<Solution> retSolution = new ArrayList<>();
+        for (Pattern p : cpSolution.getPatterns()) {
+            for (Box b : p.getAmounts()) {
+                b.setAmount(b.getAmount() + 1);
+                retSolution.add(cpSolution.clone());
+                b.setAmount(b.getAmount() - 1);
+            }
+        }
+        return retSolution;
+    }
+
+    /**
+     * Get Array of solution with -1 decrementation where it is possible.
+     *
+     * @param solution solution of reference.
+     * @return
+     */
+    private ArrayList<Solution> getRemoveNeighbourhood(final Solution solution) {
+        Solution cpSolution = solution.clone();
+        Solution modifSolution = this.getModifications(solution);
+        ArrayList<Solution> retSolution = new ArrayList<>();
+        int patternIndex = 0;
+        int boxIndex = 0;
+        for (Pattern p : cpSolution.getPatterns()) {
+            for (Box b : p.getAmounts()) {
+                if (modifSolution.getPatterns().get(patternIndex).getAmounts().get(boxIndex).getAmount() == 1) {
+                    b.setAmount(b.getAmount() - 1);
+                    retSolution.add(cpSolution.clone());
+                    b.setAmount(b.getAmount() + 1);
+                }
+                boxIndex++;
+            }
+            patternIndex++;
+            boxIndex = 0;
+        }
+        return retSolution;
     }
 }
---
+++ Core/src/test/java/com/polytech4A/cuttingstock/core/solver/IncrementNeighbourTest.java
@@ -0,0 +1,91 @@
+/*
+ *
+ *  * Project to resolve 2D cutting stock problem for Discreet Optimizations course at Polytech Lyon
+ *  * Copyright (C) 2015.  CARON Antoine and CHAUSSENDE Adrien
+ *  *
+ *  * This program is free software: you can redistribute it and/or modify
+ *  * it under the terms of the GNU Affero General Public License as
+ *  * published by the Free Software Foundation, either version 3 of the
+ *  * License, or (at your option) any later version.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  * GNU Affero General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU Affero General Public License
+ *  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.polytech4A.cuttingstock.core.solver;
+
+import com.polytech4A.cuttingstock.core.model.Box;
+import com.polytech4A.cuttingstock.core.model.Pattern;
+import com.polytech4A.cuttingstock.core.model.Solution;
+import com.polytech4A.cuttingstock.core.model.Vector;
+import com.polytech4A.cuttingstock.core.solver.neighbour.IncrementNeighbour;
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Antoine CARON on 08/04/2015.
+ *
+ * @author Antoine CARON
+ * @version 1.0
+ *          <p>
+ *          TestCase for Increment Neighbour.
+ */
+public class IncrementNeighbourTest {
+
+    private Solution solution;
+
+    private IncrementNeighbour generator;
+
+    @Before
+    public void setUp() throws Exception {
+        ArrayList<Pattern> patterns = new ArrayList<Pattern>();
+        ArrayList<Box> solutionBoxes = new ArrayList<Box>();
+        solutionBoxes.add(new Box(new Vector(4, 2), 1));
+        solutionBoxes.add(new Box(new Vector(4, 5), 2));
+        solutionBoxes.add(new Box(new Vector(4, 3), 0));
+        ArrayList<Box> solutionBoxes1 = new ArrayList<Box>();
+        solutionBoxes1.add(new Box(new Vector(4, 2), 1));
+        solutionBoxes1.add(new Box(new Vector(4, 5), 7));
+        solutionBoxes1.add(new Box(new Vector(4, 3), 0));
+        ArrayList<Box> solutionBoxes2 = new ArrayList<Box>();
+        solutionBoxes2.add(new Box(new Vector(4, 2), 0));
+        solutionBoxes2.add(new Box(new Vector(4, 5), 4));
+        solutionBoxes2.add(new Box(new Vector(4, 3), 1));
+        patterns.add(new Pattern(new Vector(4, 9), solutionBoxes));
+        patterns.add(new Pattern(new Vector(4, 9), solutionBoxes1));
+        patterns.add(new Pattern(new Vector(4, 9), solutionBoxes2));
+        solution = new Solution(patterns);
+        generator = new IncrementNeighbour();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        solution = null;
+        generator = null;
+    }
+
+    @Test
+    public void testGetModifications() throws Exception {
+        Solution solutions = generator.getModifications(solution);
+        TestCase.assertNotNull(solutions);
+        TestCase.assertEquals(solution.getPatterns().size(), solutions.getPatterns().size());
+    }
+
+    @Test
+    public void testGetNeighbourhood() throws Exception {
+        ArrayList<Solution> solutions = generator.getNeighbourhood(solution);
+        int count = solution.getPatterns().parallelStream().mapToInt(p -> p.getAmounts().size()).sum();
+        count += solution.getPatterns().parallelStream().mapToInt(p -> (int) p.getAmounts().parallelStream().filter(b -> b.getAmount() > 0).count()).sum();
+        TestCase.assertEquals(count, solutions.size());
+    }
+}
